c3444ad2d10bbb964e198feeb5ba7acf4dc75fc7
datFlowers <diegotamez05@hotmail.com>
2026-04-08 21:53:06 -0600

    Initial Commit

---
+++ ChangeMakers2026 iSleepUntilWWDC/ChangeMakers2026_iSleepUntilWWDCApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChangeMakers2026_iSleepUntilWWDCApp.swift
+//  ChangeMakers2026 iSleepUntilWWDC
+//
+//  Created by Alumno on 08/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ChangeMakers2026_iSleepUntilWWDCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChangeMakers2026 iSleepUntilWWDC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ChangeMakers2026 iSleepUntilWWDC
+//
+//  Created by Alumno on 08/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
